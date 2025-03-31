501bdbd707eae5c7779d1906ab21a958c50de797
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-03-31 15:31:35 -0400

    renamed gorup to big inputs

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -8,7 +8,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("10K Input", |b| b.iter(|| fib(black_box(10000))));
     c.bench_function("100K Input", |b| b.iter(|| fib(black_box(100000))));
     {
-        let mut group = c.benchmark_group("1M Input");
+        let mut group = c.benchmark_group("Big Inputs");
         group.sample_size(10);
         group.bench_function("1M Input", |b| b.iter(|| fib(black_box(1000000))));
     }
