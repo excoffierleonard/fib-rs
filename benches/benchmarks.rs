f807842ae0c365192e14703f83bab0257436e738
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-03-31 19:38:47 -0400

    now calculate significant thresholds of the algorithm.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,7 +1,9 @@
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use fib::fib;
+use num_format::ToFormattedString;
+use std::time::{Duration, Instant};
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn basics(c: &mut Criterion) {
     c.bench_function("Max u128 Calculation", |b| b.iter(|| fib(black_box(185))));
     c.bench_function("Max u128 Output", |b| b.iter(|| fib(black_box(186))));
     c.bench_function("Max u8 Input", |b| b.iter(|| fib(black_box(255))));
@@ -16,5 +18,44 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn thresholds(_c: &mut Criterion) {
+    // One frame at 60 FPS
+    calculate_threshold(16, 1000);
+    // One frame at 120 FPS
+    calculate_threshold(8, 1000);
+    // One frame at 144 FPS
+    calculate_threshold(6, 1000);
+    // One frame at 240 FPS
+    calculate_threshold(4, 1000);
+    // One millisecond
+    calculate_threshold(1, 1000);
+}
+
+fn calculate_threshold(target_duration_ms: u64, step: u128) {
+    let target_duration = Duration::from_millis(target_duration_ms);
+    let mut n = 0;
+    let mut last_n_below_threshold = n;
+
+    loop {
+        let start = Instant::now();
+        let _result = fib(n);
+        let duration = start.elapsed();
+
+        if duration > target_duration {
+            break;
+        }
+
+        last_n_below_threshold = n;
+        n += step;
+    }
+
+    println!(
+        "Last input 'n' before exceeding {} ms (step={}): {}",
+        target_duration_ms,
+        step,
+        last_n_below_threshold.to_formatted_string(&num_format::Locale::en)
+    );
+}
+
+criterion_group!(benches, basics, thresholds);
 criterion_main!(benches);
