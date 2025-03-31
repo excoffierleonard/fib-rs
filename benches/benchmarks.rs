42e44e0fd009446d35cfe875456d7dedeaec21f8
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-03-31 15:30:16 -0400

    dedicated one benchmark for 1M input

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -7,7 +7,11 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("1K Input", |b| b.iter(|| fib(black_box(1000))));
     c.bench_function("10K Input", |b| b.iter(|| fib(black_box(10000))));
     c.bench_function("100K Input", |b| b.iter(|| fib(black_box(100000))));
-    c.bench_function("1M Input", |b| b.iter(|| fib(black_box(1000000))));
+    {
+        let mut group = c.benchmark_group("1M Input");
+        group.sample_size(10);
+        group.bench_function("1M Input", |b| b.iter(|| fib(black_box(1000000))));
+    }
 }
 
 criterion_group!(benches, criterion_benchmark,);
