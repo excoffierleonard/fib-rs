08882de9c68216d64118ecdc122f9baa4e3c02f9
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-04-15 00:26:53 -0400

    working state web front

--- src/bin/web.rs
+++ src/bin/web.rs
@@ -1,4 +1,4 @@
-use fib_rs::{fib, fib_range};
+use fib_rs::fib;
 
 use leptos::mount::mount_to_body;
 use leptos::prelude::*;
@@ -9,9 +9,24 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
+    let (value, set_value) = signal(Ok(0));
+    let (result, set_result) = signal(String::new());
+
+    let calculate = move |_| match value.get() {
+        Ok(n) => set_result.set(format!("F({}) = {}", n, fib(n))),
+        Err(_) => set_result.set("Please enter a valid number".to_string()),
+    };
+
     view! {
         <div>
             <h1>"Fibonacci Calculator"</h1>
+            <input
+                type="number"
+                placeholder="Enter a number"
+                on:input:target=move |ev| { set_value.set(ev.target().value().parse::<u128>()) }
+            />
+            <button on:click=calculate>"Calculate"</button>
+            <p style="white-space: pre-wrap; word-wrap: break-word;">{result}</p>
         </div>
     }
 }
